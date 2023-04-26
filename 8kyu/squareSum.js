463ffbc2a3013e36f1b94313cb33ab23b81aa5bc
Alexei Rusu <rusualexei1995@gmail.com>
2023-04-26 19:06:04 -0400

    square sum

---
+++ 8kyu/squareSum.js
@@ -0,0 +1,9 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 
+
+function squareSum(numbers){
+    return numbers.length ? numbers.reduce((a, b) => a  + b ** 2 ) : 0
+
+}
+console.log(squareSum([1, 2, 2]))
