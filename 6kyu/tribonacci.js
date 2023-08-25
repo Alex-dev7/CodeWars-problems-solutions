f50a2017b79f5ef7e6cafcb206a56e8946357d00
Alexei Rusu <rusualexei1995@gmail.com>
2023-08-25 10:27:10 -0400

    tribonacci

--- 6kyu/tribonacci.js
+++ 6kyu/tribonacci.js
@@ -1,13 +1,26 @@
 function tribonacci(signature,n){
-    //your code here
+  //your code here
     const arr = [...signature]
+    const newArr = []
     if(n == 0 )return []
-  
-    for(let i = arr.length - 3; i <= n ; i++){
-      if(arr[i])arr.push(arr[i] + arr[i + 1] + arr[i + 2])
-      if(arr.length === n ) return arr
-    }
- 
-  }
 
-  console.log(tribonacci([1,1,1], 10)) // output [1,1,1,3,5,9,17,31,57,105]
+    for(let i = arr.length - 3; i < n ; i++){
+      
+      let next = arr[i] + arr[i + 1] + arr[i + 2]
+      
+      if(n < 4) newArr.push(signature[i])
+
+      if(next === 0){
+        arr.push(0)
+      } else {
+        arr.push(next)
+      }
+      
+      if(arr[i] <= arr[i + 1] <= arr[i + 2] || next === 0){
+        if(arr.length === n ) return arr
+      } 
+    }
+  return newArr
+}
+
+  console.log(tribonacci([ 1,1,1], 10)) // output [1,1,1,3,5,9,17,31,57,105]
